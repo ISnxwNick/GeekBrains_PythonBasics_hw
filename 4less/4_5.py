730cc520ee3a53a9b5d04d69ee529804dbedee85
Nikita Perezhogin <76184020+ISnxwNick@users.noreply.github.com>
2021-01-08 19:47:56 +0500

    Update 4_5.py

--- 4less/4_5.py
+++ 4less/4_5.py
@@ -4,3 +4,12 @@
 результат вычисления произведения всех элементов списка.
 Подсказка: использовать функцию reduce().
 """
+from functools import reduce
+
+
+def my_func(prev_el, el):
+    return prev_el * el
+
+
+my_list = [el for el in range(100, 1001) if el % 2 == 0]
+print(reduce(my_func, my_list))
