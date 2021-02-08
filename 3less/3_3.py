ae6697fa69104d1df7e079260d415244c0449717
Nikita Perezhogin <76184020+ISnxwNick@users.noreply.github.com>
2021-02-08 14:30:10 +0500

    Update 3_3.py

--- 3less/3_3.py
+++ 3less/3_3.py
@@ -8,11 +8,11 @@ def my_func(*args):
     a = [*args]
     a.sort()
     summa = a[-2] + a[-1]
-    return print(f'Сумма наибольших 2х аргументов: {summa}')
+    return f'Сумма наибольших 2х аргументов: {summa}'
 
 
 try:
     x, y, z = map(int, input('Введите 3 числа через пробел: ').split())
-    my_func(x, y, z)
+    print(my_func(x, y, z))
 except ValueError:
     print('Вы ввели не число')
